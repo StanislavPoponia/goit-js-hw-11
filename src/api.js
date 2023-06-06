9beb5ee3f17e7b737a32b349ed340e817f22358d
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-06-06 15:46:35 +0300

    new export

--- src/api.js
+++ src/api.js
@@ -7,3 +7,11 @@ export const apiOptions = {
     API_KEY: '37073976-b2cdc1fed064addf83f6fd0ff',
     page: 1,
   };
+
+  export const refs = {
+    gallary: document.querySelector('.gallery'),
+    inputSearch: document.querySelector('input[name="searchQuery"]'),
+    form: document.querySelector('#search-form'),
+    btnMore: document.querySelector('.load-more'),
+  };
+
