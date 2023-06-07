5cba468971ec11304e5421308c03d52b8d712207
StanislavPoponia <127778439+StanislavPoponia@users.noreply.github.com>
2023-06-07 17:31:20 +0300

    RGB

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import fetchApiImages from './api.js';
 import { refs, apiOptions } from './api.js';
+
+refs.form.addEventListener('submit', async e => {
+    e.preventDefault();
+  
+    refs.gallary.innerHTML = '';
+    refs.loadMore.classList.add('none');
+    apiOptions.page = 1;
+  
+    await fetchApiImages(apiOptions.API_KEY, apiOptions.page);
+  });
+  
+  refs.loadMore.addEventListener('click', async () => {
+    apiOptions.page += 1;
+  
+    await fetchApiImages(apiOptions.API_KEY, apiOptions.page);
+  });
